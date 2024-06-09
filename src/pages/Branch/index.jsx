59eb9298bdd26218d04ba2bb9534025b2e464099
Shyam-N7 <shyamsantu47@gmail.com>
2024-06-09 15:04:05 +0530

    start1

--- src/pages/Branch/index.jsx
+++ src/pages/Branch/index.jsx
@@ -4,68 +4,17 @@ import { useNavigate } from "react-router-dom";
 export default function BranchSelection() {
   const navigate = useNavigate();
 
-  const buttonAnimation = () => {
-    const messages = document.querySelectorAll(".home_buttons");
-        messages.forEach((message) => {
-          if (message) {
-            message.style.transform = "scale(1.07)";
-            message.style.boxShadow = "0 0 8px 8px rgba(0, 0, 0, 0.349)";
-            message.style.outline = "3px solid #b0c4b186";
-      
-            setTimeout(() => {
-              message.style.transform = "scale(1)";
-              message.style.boxShadow = "none";
-              message.style.outline = "none";
-            }, 300);
-          }
-        });
-      };
-
   const navigateWithDelay = () => {
     setTimeout(() => {
       navigate("/year_selection");
     }, 500);
   };
-
-  const buttonAnimation_1 = () => {
-    const messages = document.querySelectorAll(".home_button_1");
-        messages.forEach((message) => {
-          if (message) {
-            message.style.transform = "scale(1.07)";
-            message.style.boxShadow = "0 0 8px 8px rgba(0, 0, 0, 0.349)";
-            message.style.outline = "3px solid #b0c4b186";
-      
-            setTimeout(() => {
-              message.style.transform = "scale(1)";
-              message.style.boxShadow = "none";
-              message.style.outline = "none";
-            }, 300);
-          }
-        });
-      };
-
+  
   const navigateWithDelay_1 = () => {
     setTimeout(() => {
       navigate("/year_selection");
     }, 500);
   };
-
-  const buttonAnimation_2 = () => {
-    const messages = document.querySelectorAll(".home_button_2");
-        messages.forEach((message) => {
-          if (message) {
-            message.style.transform = "scale(1.07)";
-            message.style.boxShadow = "0 0 8px 8px rgba(0, 0, 0, 0.349)";
-            message.style.outline = "3px solid #b0c4b186";
-      
-            setTimeout(() => {
-              message.style.transform = "scale(1)";
-              message.style.boxShadow = "none";
-              message.style.outline = "none";
-            }, 300);
-          }
-        });
-      };
 
   const navigateWithDelay_2 = () => {
     setTimeout(() => {
@@ -79,38 +28,38 @@ export default function BranchSelection() {
     <div className="Homepage">
       <div className="branch_title">Select your branch?</div>
       <div className="Buttons">
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           CSE
         </button>
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           ISE
         </button>
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           ECE
         </button>
-        <button className="home_buttons" onClick={() => {setCurrentSelection(1); buttonAnimation(); }}>
+        <button className="home_buttons" onClick={() => {setCurrentSelection(1);  }}>
           CSE - AI
         </button>
         {currentSelection === 1 && (
           <div>
-            <button onClick={() => { buttonAnimation_1(); navigateWithDelay_1(); }} className="home_button_1">
+            <button onClick={() => {  navigateWithDelay_1(); }} className="home_button_1">
               ML
             </button>
-            <button onClick={() => { buttonAnimation_2(); navigateWithDelay_2(); }} className="home_button_2">
+            <button onClick={() => {  navigateWithDelay_2(); }} className="home_button_2">
               DS
             </button>
           </div>
         )}
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           EEE
         </button>
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           ME
         </button>
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} className="home_buttons">
           CV
         </button>
-        <button onClick={() => { buttonAnimation(); navigateWithDelay(); }} disabled className="home_buttons">
+        <button onClick={() => {  navigateWithDelay(); }} disabled className="home_buttons">
           CS
         </button>
       </div>
